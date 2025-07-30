83e42b8cd01496c455eeb802764a882a18b9bb03
Bhargav ram <88724500+Padidapu-Bhargav@users.noreply.github.com>
2025-07-30 14:17:52 +0530

    Update Single_port_buffer.sv

--- PING_PONG_BUFFER/Single_port_buffer.sv
+++ PING_PONG_BUFFER/Single_port_buffer.sv
@@ -1,43 +1,43 @@
 // working and invoking BRAMS
 // 
-// module Single_port_buffer #(
+module Single_port_buffer_rd_address #(
 	
-//     parameter dw = 56, // 28 bit real and 28 bit imaginary
-//     parameter buffer_depth = 1440,
-//     parameter Add_width = $clog2(buffer_depth)
-// )
-//     (
-//     input 					       clk,
-//     input 					       rst,
+    parameter dw = 56, // 28 bit real and 28 bit imaginary
+    parameter buffer_depth = 1440,
+    parameter Add_width = $clog2(buffer_depth)
+)
+    (
+    input 					       clk,
+    input 					       rst,
 
-//     input   logic [Add_width-1       :           0] wr_address,
-//     input   logic [dw-1              :           0] wr_data,
-//     input   logic                                   wr_en,
+    input   logic [Add_width-1       :           0] wr_address,
+    input   logic [dw-1              :           0] wr_data,
+    input   logic                                   wr_en,
 
-//     input   logic [Add_width-1       :           0] rd_address,
-//     output  logic [dw-1              :           0] rd_data,
-//     input   logic                                   rd_en
-// );
+    input   logic [Add_width-1       :           0] rd_address,
+    output  logic [dw-1              :           0] rd_data,
+    input   logic                                   rd_en
+);
 
-// (* ramstyle = "M20K" *) logic [dw-1:0] bram [buffer_depth];
+(* ramstyle = "M20K" *) logic [dw-1:0] bram [buffer_depth];
 
 
-// always_ff @( posedge clk  ) begin : write_data_logic
-//     // writing data
-//     if (wr_en) begin
-//         bram[wr_address]	<= wr_data;
-//     end
-//     // reading data
-//     if (rd_en) begin
-// 	    rd_data 		<= bram[rd_address];
-//     end
-// end
+always_ff @( posedge clk  ) begin : write_data_logic
+    // writing data
+    if (wr_en) begin
+        bram[wr_address]	<= wr_data;
+    end
+    // reading data
+    if (rd_en) begin
+	    rd_data 		<= bram[rd_address];
+    end
+end
 
-// endmodule
+endmodule
 
 
 // working for output rd_Addres
-module Single_port_buffer #(
+module Single_port_buffer_rd_enable #(
 	
     parameter dw = 56, // 28 bit real and 28 bit imaginary
     parameter buffer_depth = 1440,
